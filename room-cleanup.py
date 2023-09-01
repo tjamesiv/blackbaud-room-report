049473d98f6d81b6eb4e89d66866c72ad3bf6063
Tom James <tom.james.iv@gmail.com>
2023-09-01 15:29:22 -0400

    Works but the classes are actually above the rooms in the input file.

--- room-cleanup.py
+++ room-cleanup.py
@@ -1,9 +1,12 @@
 import csv
 
 # Read the CSV file and create a dictionary to store room information
-rooms = {}
+rooms_report = {}
 current_room = None
 blocks = []
+
+need_to_handle_first_class = False
+first_class = None
 
 with open('input.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -12,29 +15,50 @@ with open('input.csv', 'r') as csv_file:
         # Grab the list of blocks
         if row_number == 4:
             blocks = row
-            print(blocks)
 
         # If there's a new room entry
         if row[1]:
+
+            print(f"Found room {row[1]}")
+
             current_room = row[1]
-            rooms[current_room] = []
+            rooms_report[current_room] = []
             # blocks = [block.strip() for block in row[4:] if block.strip()]
+
         # If there's a class
-        if row[2]:
-            non_blank_columns = [index for index, value in enumerate(row) if value != ""]
-            if len(non_blank_columns) == 1:
-                column_index = non_blank_columns[0] # This is the column where the block is for that class
-                block = row[4]
+        if row[2] or need_to_handle_first_class:
 
-                # Need to decide data structure to associate each room with a block/teacher/enrollment triple
+            if need_to_handle_first_class:
+                need_to_handle_first_class = False
 
-        if current_room and row[i + offset]:
-            instructor = row[i]
-            rooms[current_room].append(f"Block {blocks.pop(0)}: {instructor}")
+            print(f"Found class {row[2]}")
+
+            # Deal with weird formatting for first class
+            if current_room is None:
+                print("a")
+                first_class = row[2]
+                need_to_handle_first_class = True
+                
+            else:
+                column_index = None
+                non_blank_count = 0
+                # Iterate through the row elements and find the second non-blank entry
+                for index, entry in enumerate(row):
+                    if entry.strip() != "":
+                        non_blank_count += 1
+                        if non_blank_count == 2:
+                            column_index = index  # Index of the second non-blank entry
+                            break
+
+                if column_index is not None:
+                    course = row[2]
+                    block = blocks[column_index]
+                    teacher = row[column_index]
+                    rooms_report[current_room].append(f"Block {block}: {course} ({teacher})")
 
 # Write room information to a text file
 with open('room_schedule.txt', 'w') as txt_file:
-    for room, blocks in rooms.items():
+    for room, blocks in rooms_report.items():
         txt_file.write(f"{room}\n\n")
         for block_info in blocks:
             txt_file.write(f"{block_info}\n")
